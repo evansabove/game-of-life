5ba84f7be90aaade5c7b8688bc361ffd39ac3da6
Andrew Evans <AEvans@seamsglobal.com>
2019-05-31 10:40:41 +0100

    Fixing tslint warnings.

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import GameOfLife from "./components/GameOfLife.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import GameOfLife from './components/GameOfLife.vue';
 
 @Component({
   components: {
